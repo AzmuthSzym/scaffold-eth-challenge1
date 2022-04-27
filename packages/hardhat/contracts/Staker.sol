71b72c45881f43a5851a9c829ab7ba959dc0325a
AzmuthSzym <simondziadek@gmail.com>
2022-04-27 23:53:30 +0200

    Stake funcion update

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.4;
+pragma solidity ^0.8.4;
 
 import "hardhat/console.sol";
 import "./ExampleExternalContract.sol";
@@ -7,7 +7,10 @@ import "./ExampleExternalContract.sol";
 contract Staker {
 
   ExampleExternalContract public exampleExternalContract;
+  mapping(address => uint256) public balances;
+  uint256 public constant threshold = 1 ether;
 
+  event Stake(address indexed sender, uint256 amount);
   constructor(address exampleExternalContractAddress) public {
       exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
   }
@@ -15,7 +18,13 @@ contract Staker {
   // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
   //  ( make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
 
-
+  function stake() public payable {
+    // update the user's balance
+    balances[msg.sender] += msg.value;
+    
+    // emit the event to notify the blockchain that we have correctly Staked some fund for the user
+    emit Stake(msg.sender, msg.value);
+  }
   // After some `deadline` allow anyone to call an `execute()` function
   //  It should either call `exampleExternalContract.complete{value: address(this).balance}()` to send all the value
 
